5fdc4b629dd59f7879ad2bca85b5dbd3d1648ffe
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2022-08-27 00:25:26 +0530

    script added, changes need to be made

---
+++ Brackeys Game Jam/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private GameObject player;
+    private Rigidbody enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = -(transform.position - player.transform.position).normalized;
+        enemy.AddForce(direction * speed);
+    }
+}
