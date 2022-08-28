e5fedc6c42faea825a0c76119067260f1e8a8094
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2022-08-28 11:25:50 +0530

    all done

---
+++ Brackeys Game Jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    private Transform playerTransform;
+    public List<GameObject> enemies;
+    public TextMeshProUGUI timer;
+    private float timerTime = 60.0f;
+    private bool isGameActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        isGameActive = true;
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerTime -= Time.deltaTime;
+        Mathf.Round(timerTime);
+        timer.text = "TIMER:\n " + timerTime;
+        if(timerTime<0)
+        {
+            isGameActive = false;
+            Debug.Log("You Survived!");
+            //GameOver();
+        }    
+    }
+    IEnumerator Spawn()
+    {
+        while (isGameActive)
+        {
+            float randomX = Random.Range(playerTransform.position.x + 5, playerTransform.position.x + 10);
+            float randomZ = Random.Range(playerTransform.position.z + 5, playerTransform.position.z + 10);
+            Vector3 randomPos = new Vector3(randomX, 0.5f, randomZ);
+            int randomIndex = Random.Range(0, 3);
+            if (isGameActive)
+            {
+                Instantiate(enemies[randomIndex], randomPos, enemies[randomIndex].transform.rotation);
+            }
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
---
+++ Brackeys Game Jam/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private Transform playerTransform;
+    public List<GameObject> enemyPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    IEnumerator Spawn()
+    {    
+        float randomX = Random.Range(playerTransform.position.x + 5, playerTransform.position.x + 10);
+        float randomZ = Random.Range(playerTransform.position.z + 5, playerTransform.position.z + 10);
+        Vector3 randomPos = new Vector3(randomX, 0.5f, randomZ);
+        int randomIndex = Random.Range(0, 3);
+
+        yield return new WaitForSeconds(2);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
