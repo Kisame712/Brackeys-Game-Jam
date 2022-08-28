5989968fb07ce0f2dff9ed7629d8faec724adde5
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2022-08-28 14:25:58 +0530

    done :)

--- Brackeys Game Jam/Assets/Scripts/GameManager.cs
+++ Brackeys Game Jam/Assets/Scripts/GameManager.cs
@@ -9,13 +9,21 @@ public class GameManager : MonoBehaviour
     private Transform playerTransform;
     public List<GameObject> enemies;
     public TextMeshProUGUI timer;
+    public GameObject winText;
+    public GameObject loseText;
     public float timerTime = 60.0f;
     public bool isGameActive;
-    public GameObject WinPanel;
-    public GameObject LosePanel;
     public void GameOver()
     {
-        WinPanel.SetActive(true);
+        winText.SetActive(true);
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Back();
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            Restart();
+        }
     }
 
     public void Restart()
@@ -70,7 +78,15 @@ public class GameManager : MonoBehaviour
     }
     public void Death()
     {
-        LosePanel.SetActive(true);
+        loseText.SetActive(true);
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Back();
+        }
+        else if(Input.GetKey(KeyCode.R))
+        {
+            Restart();
+        }
     }
 
 }  
